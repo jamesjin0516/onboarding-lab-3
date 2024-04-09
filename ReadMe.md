cf152c9eae836ff612343387ae2b51f472095bfd
Ge(James) Jin <gj2148@nyu.edu>
2024-04-09 13:15:52 -0400

    exercise2: 16 cases passed

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,4 @@
 # Week 3 Lab: Verification
 
-Fill out this readme as required by lab guidance
+## Exercise 2 Testing Strategy
+Since this test is to verify correct behaviour after module modification, I decided to test the reset mechanism and the least significant bit generation. I iterated through all 16 combinations of the four bits producing the least siginificant bit and checked for correct output after one clock cycle. Since bit shift is relible, this strategy ensures that the module behaves correctly for all possible inputs.
--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,5 +1,50 @@
+#include <cstdint>
+#include <iostream>
 #include <VExercise2.h>
 
 int main() {
   VExercise2 model;
+  uint8_t critbits = 0;
+  uint8_t success = 0;
+  // Test each combination of the 4 bits participating in XOR operation
+  while (critbits < 16) {
+    uint16_t input = 0;
+    // Set input according to current combination
+    if (critbits & 1) input |= 2;
+    if (critbits & 2) input |= 8;
+    if (critbits & 4) input |= 256;
+    if (critbits & 8) input |= 1024;
+    // Test reset
+    model.reset = 1;
+    model.clk = 1;
+    model.init = ~input;
+    model.eval();
+    model.clk = 0;
+    model.eval();
+    if (model.out != input) {
+      std::cerr << "Reset failed. Output: " << model.out << "; expected " << input << std::endl;
+    // Test shift if reset succeeded
+    } else {
+      model.reset = 0;
+      model.clk = 1;
+      model.eval();
+      model.clk = 0;
+      model.eval();
+      uint16_t next_bit = ((input >> 1) & 1) ^ ((input >> 3) & 1) ^ ((input >> 8) & 1) ^ ((input >> 10) & 1);
+      input = input << 1;
+      input |= next_bit;
+      if (model.out != input) {
+        std::cerr << "Shift failed. Output: " << model.out << "; expected " << input << std::endl;
+      } else {
+        ++success;
+      }
+    }
+    ++critbits;
+  }
+  // Report number of failed cases, if any.
+  if (success != critbits) {
+    std::cerr << static_cast<unsigned>(critbits - success) << " out of " << static_cast<unsigned>(critbits) << " test cases failed." << std::endl;
+    return 1;
+  }
+  std::cout << "All " << static_cast<unsigned>(success) << " test cases passed." << std::endl;
 }
