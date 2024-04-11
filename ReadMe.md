43804a6903d1c12acb44b52de4d1ee5a3e467f77
Ge(James) Jin <gj2148@nyu.edu>
2024-04-11 14:37:47 -0400

    exercise4: 67108868 cases passed

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,6 @@
 
 ## Exercise 2 Testing Strategy
 Since this test is to verify correct behaviour after module modification, I decided to test the reset mechanism and the least significant bit generation. I iterated through all 16 combinations of the four bits producing the least siginificant bit and checked for correct output after one clock cycle. Since bit shift is relible, this strategy ensures that the module behaves correctly for all possible inputs.
+
+## Exercise 4 Testing Igored Value
+Since all possible sel values should produce 0 when cs is 0, I can simply verify this by looping through all sel values (0 to 3) and check that out is 0 given cs is fixed to 0.
--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,5 +1,48 @@
+#include <iostream>
 #include <VExercise4.h>
 
 int main() {
   VExercise4 model;
+  unsigned fails = 0;
+  unsigned cases = 0;
+  model.cs = 0;
+  // Ensure any sel value and cs = 0 produce out = 0
+  for (model.sel = 0; model.sel < 4; ++model.sel) {
+    model.eval();
+    if (model.out != 0) {
+      std::cerr << "Zero cs produce nonzero output: " << model.out << std::endl;
+      ++fails;
+    }
+    ++cases;
+  }
+  model.cs = 1;
+  model.alpha = 0;
+  model.beta = 0;
+  model.gamma = 0;
+  // Test all input combinations between alpha, beta, and gamma
+  do {
+    do {
+      do {
+        // Test all sel values given current combinations of inputs
+        for (model.sel = 0; model.sel < 4; ++model.sel) {
+          model.eval();
+          uint8_t expected;
+          if (model.sel == 0) expected = model.alpha;
+          else if (model.sel == 1) expected = model.beta;
+          else if (model.sel == 2) expected = model.gamma;
+          else if (model.sel == 3) expected = model.alpha & (model.beta | model.gamma);
+          if (model.out != expected) {
+            std::cerr << "sel = " << model.sel << " failed, output: " << model.out << "; expected: " << expected << std::endl;
+            ++fails;
+          }
+          ++cases;
+        }
+      } while (++model.gamma);
+    } while (++model.beta);
+  } while (++model.alpha);
+  if (fails > 0) {
+    std::cerr << fails << " out of " << cases << " tests failed" << std::endl;
+    return 1;
+  }
+  std::cout << "All " << cases << " tests passed" << std::endl;
 }
